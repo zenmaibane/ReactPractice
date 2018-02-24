03b3c031978ebc6695c90613f472954d90d7e395
zenmaibane <koteicream.1024@gmail.com>
2018-02-24 18:12:13 +0900

    こーれがstateの変化かっつってる

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -5,15 +5,29 @@ export interface ToDo {
     text: string;
 }
 
-class ToDoView extends React.Component<ToDo> {
+class ToDoView extends React.Component<ToDo, ToDo> {
     constructor(props: ToDo) {
         super(props);
+        this.state = { id: props.id, isCompleted: props.isCompleted, text: props.text };
+        this.completedTask = this.completedTask.bind(this);
+    }
+    completedTask() {
+        this.setState({ isCompleted: true });
+    }
+    showCompletedLink() {
+        if (this.state.isCompleted) {
+            return;
+        }
+        return (
+            <a href="#" onClick={this.completedTask}>終わった</a>
+        );
     }
     render() {
         return (
-            <p>
+            <div>
                 {this.props.text}
-            </p>
+                {this.showCompletedLink()}
+            </div>
         );
     }
 }
